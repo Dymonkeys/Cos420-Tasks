6d68c1deea937590869eab64ce7643c41f95b3f0
dymonkeys <20neufeldd@gmail.com>
2023-09-10 12:02:11 -0400

    Completed task 2

--- src/arrays.ts
+++ src/arrays.ts
@@ -14,8 +14,10 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const tripledNumbers: number[] = numbers.map(number => number * 3);
+    return tripledNumbers;
 }
+
 
 /**
  * Consume an array of strings and convert them to integers. If
