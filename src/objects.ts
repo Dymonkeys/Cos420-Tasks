d6bba65fd1dc6c9356f33315884b3d112b90322c
dymonkeys <20neufeldd@gmail.com>
2023-09-21 11:37:45 -0400

    Compleated makeBlankQuestion

--- src/objects.ts
+++ src/objects.ts
@@ -1,17 +1,27 @@
 import { Question, QuestionType } from "./interfaces/question";
 
 /**
- * Create a new blank question with the given `id`, `name`, and `type. The `body` and
- * `expected` should be empty strings, the `options` should be an empty list, the `points`
- * should default to 1, and `published` should default to false.
+ * Create a new blank question with the given `id`, `name`, and `type`.
+ * The `body` and `expected` should be empty strings, the `options` should be an empty list,
+ * the `points` should default to 1, and `published` should default to false.
  */
 export function makeBlankQuestion(
     id: number,
     name: string,
     type: QuestionType
 ): Question {
-    return {};
+    return {
+        id,
+        name,
+        type,
+        body: "",
+        expected: "",
+        options: [],
+        points: 1,
+        published: false
+    };
 }
+
 
 /**
  * Consumes a question and a potential `answer`, and returns whether or not
