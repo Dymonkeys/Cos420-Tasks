c1cb54292895de8fd3bcd816da2e8b42cca08be2
dymonkeys <20neufeldd@gmail.com>
2023-10-01 18:21:25 -0400

    Finished Task 12

--- src/nested.ts
+++ src/nested.ts
@@ -1,5 +1,6 @@
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
+import { makeBlankQuestion } from "./objects";
 
 /**
  * Consumes an array of questions and returns a new array with only the questions
@@ -149,7 +150,9 @@ export function addNewQuestion(
     name: string,
     type: QuestionType
 ): Question[] {
-    return [];
+    const out = [...questions];
+    out.push(makeBlankQuestion(id, name, type));
+    return out;
 }
 
 /***
