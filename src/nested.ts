216b3ee26b3cc5ce5930d4a77b7a0ebb5ee8460f
dymonkeys <20neufeldd@gmail.com>
2023-10-01 18:02:48 -0400

    completed Task 10

--- src/nested.ts
+++ src/nested.ts
@@ -121,7 +121,11 @@ export function makeAnswers(questions: Question[]): Answer[] {
  * each question is now published, regardless of its previous published status.
  */
 export function publishAll(questions: Question[]): Question[] {
-    return [];
+    return questions.map((question) => {
+        const newQuestion = { ...question };
+        newQuestion.published = true;
+        return newQuestion;
+    });
 }
 
 /***
