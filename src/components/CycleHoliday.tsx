233a8251cebed7934ad470a35096f30ad6a38123
David Neufeld <20neufeldd@gmail.com>
2023-10-11 16:36:11 -0400

    Cycle Holiday Completed

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,42 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const holidays = [
+        { emoji: "🎏", name: "Dragon Boat Festival" },
+        { emoji: "🎃", name: "Halloween" },
+        { emoji: "🎄", name: "Christmas" },
+        { emoji: "🎂", name: "Birthday" },
+        { emoji: "🪔", name: "Diwali" }
+        // Add more holidays
+    ];
+
+    const [currentHoliday, setCurrentHoliday] = useState(holidays[0]);
+
+    const getNextHolidayAlphabetically = () => {
+        const currentIndex = holidays.findIndex(
+            (holiday) => holiday.name === currentHoliday.name
+        );
+        const nextIndex =
+            currentIndex === holidays.length - 1 ? 0 : currentIndex + 1;
+        setCurrentHoliday(holidays[nextIndex]);
+    };
+
+    const getNextHolidayInYear = () => {
+        const currentIndex = holidays.findIndex(
+            (holiday) => holiday.name === currentHoliday.name
+        );
+        const nextIndex =
+            currentIndex === holidays.length - 1 ? 0 : currentIndex + 1;
+        setCurrentHoliday(holidays[nextIndex]);
+    };
+
+    return (
+        <div>
+            <div>Holiday: {currentHoliday.emoji}</div>
+            <Button onClick={getNextHolidayAlphabetically}>
+                Advance by Alphabet
+            </Button>
+            <Button onClick={getNextHolidayInYear}>Advance by Year</Button>
+        </div>
+    );
 }
