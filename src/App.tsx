550f7500d16881194515d61b1ad9036e204740af
David Neufeld <20neufeldd@gmail.com>
2023-10-11 15:55:42 -0400

    Added back some code from earlier into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
@@ -18,6 +18,17 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. David Neufeld
             </p>
+            <h1>Hello World</h1>
+            <img
+                src="../assets/images/pet-ada.jpg"
+                alt="A picture of my dog Ada"
+            />
+            <div>Ordered List:</div>
+            <ol>
+                <li>First thing</li>
+                <li>Another thing</li>
+                <li>A third item</li>
+            </ol>
             <Button onClick={() => console.log("Hello World!")}>
                 Log Hello World
             </Button>
