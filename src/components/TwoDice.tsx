accb2b9458592e200ce541103346c1fe54b351da
David Neufeld <20neufeldd@gmail.com>
2023-10-11 17:09:41 -0400

    Finished TwoDice

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,23 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [die1, setDie1] = useState(3);
+    const [die2, setDie2] = useState(6);
+
+    function rollLeft() {
+        setDie1(d6());
+    }
+    function rollRight() {
+        setDie2(d6());
+    }
+    return (
+        <div>
+            <span data-testid="left-die">{die1}</span>
+            <span data-testid="right-die">{die2}</span>
+            <Button onClick={rollLeft}>Roll Left</Button>
+            <Button onClick={rollRight}>Roll Right</Button>
+            {1 === die1 && 1 === die2 && <div>You Lose...</div>}
+            {die1 !== 1 && die1 === die2 && <div>You Win!</div>}
+        </div>
+    );
 }
