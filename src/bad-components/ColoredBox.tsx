86bcf3e64cb131087b98ea495a3088b7fc3d79a5
David Neufeld <20neufeldd@gmail.com>
2023-10-20 12:04:41 -0400

    Fixed both by using props

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -4,40 +4,57 @@ import { Button } from "react-bootstrap";
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
 
+interface colorIndexProps {
+    setColorIndex: (dhValue: number) => void;
+    colorIndex: number;
+}
+
+function ColorPreview({
+    setColorIndex,
+    colorIndex
+}: colorIndexProps): JSX.Element {
+    return (
+        <div
+            data-testid="colored-box"
+            style={{
+                width: "50px",
+                height: "50px",
+                backgroundColor: COLORS[colorIndex],
+                display: "inline-block",
+                verticalAlign: "bottom",
+                marginLeft: "5px"
+            }}
+        ></div>
+    );
+}
+
+function ChangeColor({
+    setColorIndex,
+    colorIndex
+}: colorIndexProps): JSX.Element {
+    return (
+        <Button onClick={() => setColorIndex((1 + colorIndex) % COLORS.length)}>
+            Next Color
+        </Button>
+    );
+}
+
 export function ColoredBox(): JSX.Element {
     const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
-    function ColorPreview(): JSX.Element {
-        return (
-            <div
-                data-testid="colored-box"
-                style={{
-                    width: "50px",
-                    height: "50px",
-                    backgroundColor: COLORS[colorIndex],
-                    display: "inline-block",
-                    verticalAlign: "bottom",
-                    marginLeft: "5px"
-                }}
-            ></div>
-        );
-    }
-    function ChangeColor(): JSX.Element {
-        return (
-            <Button
-                onClick={() => setColorIndex((1 + colorIndex) % COLORS.length)}
-            >
-                Next Color
-            </Button>
-        );
-    }
 
     return (
         <div>
             <h3>Colored Box</h3>
             <span>The current color is: {COLORS[colorIndex]}</span>
             <div>
-                <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
+                <ChangeColor
+                    colorIndex={colorIndex}
+                    setColorIndex={setColorIndex}
+                ></ChangeColor>
+                <ColorPreview
+                    colorIndex={colorIndex}
+                    setColorIndex={setColorIndex}
+                ></ColorPreview>
             </div>
         </div>
     );
